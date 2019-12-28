a079647bad8e6b7ef8cf2793332c756c01849510
shuzhiliu <liushuzhi2014@gmail.com>
2019-12-28 23:56:18 +0900

    Confirm the methods to modify and reuse pretrained model, please check the Backbone_test in Backbone.py

--- NN_Parts/Backbone.py
+++ NN_Parts/Backbone.py
@@ -2,24 +2,46 @@ import tensorflow as tf
 import inspect
 
 class Backbone_test:
-    IMG_SHAPE = (1280, 720, 3)
+    IMG_SHAPE = (224, 224, 3)
     input1 = tf.keras.Input(shape=IMG_SHAPE)
     base_model = tf.keras.applications.ResNet50V2(input_shape=IMG_SHAPE,
-                                                  include_top=False)
+                                                  include_top=True)
+    # entire pretrained model
     tf.keras.utils.plot_model(model=base_model, to_file='base_model.png', show_shapes=True)
+    # two methods to build test base_model2
+    # base_model2 = tf.keras.Model(inputs=[base_model.layers[0].output], outputs=[base_model.layers[-3].output])
+    base_model2 = tf.keras.Model(inputs=[base_model.get_layer(index=0).input], outputs=[base_model.get_layer(index=-3).output])
+    tf.keras.utils.plot_model(model=base_model2, to_file='base_model_cut.png', show_shapes=True)
+    # To build base_model3, we need input layer
+    input2 = tf.keras.Input(shape=(7,7,2048))
+    base_model3 = tf.keras.Model(inputs=[input2], outputs=[base_model.get_layer(index=-2)(input2)])
+    tf.keras.utils.plot_model(model=base_model3, to_file='base_model_cut2.png', show_shapes=True)
+    # better use Sequential API
+    base_model4 = tf.keras.Sequential(layers=[
+        input2,
+        base_model.get_layer(index=-2),
+        base_model.get_layer(index=-1)
+    ])
+    # Check if the weights are same in two models
+    print(base_model.layers[-1].get_weights()[0].flatten()[:5])
+    print(base_model4.layers[-1].get_weights()[0].flatten()[:5])
+    tf.keras.utils.plot_model(model=base_model4, to_file='base_model_cut3.png', show_shapes=True)
     # print(base_model.summary())
     conv1 = tf.keras.layers.Conv2D(filters=256, kernel_size=(1,1), padding='same')
     bh1 = tf.keras.layers.BatchNormalization()
     ac1 = tf.keras.layers.Activation(activation=tf.keras.activations.relu)
-    # model2 = tf.keras.Model(inputs=[input1], outputs=[ac1(bh1(conv1(base_model(input1))))])
-    model2 = tf.keras.Sequential(layers=[
-        input1,
-        base_model,
-        conv1,
-        bh1,
-        ac1
-    ])
+    model2 = tf.keras.Model(inputs=[input1], outputs=[ac1(bh1(conv1(base_model2(input1))))])   # try functional API
+    # Try Sequential API, better use Sequential API
+    # model2 = tf.keras.Sequential(layers=[
+    #     input1,
+    #     base_model,
+    #     conv1,
+    #     bh1,
+    #     ac1
+    # ])
     tf.keras.utils.plot_model(model=model2, to_file='base_model_modified.png', show_shapes=True)
+
+    print(len(base_model.layers))
 
 
 
@@ -53,9 +75,9 @@ class Backbone:
 
 
 if __name__=='__main__':
-    t1 = Backbone()
-    t1.plot_model()
-    print(t1.get_output_shape())
+    t1 = Backbone_test()
+    # t1.plot_model()
+    # print(t1.get_output_shape())
 
 
 
