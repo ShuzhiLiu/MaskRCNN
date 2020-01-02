9bce7cedd0e8d2388595af4b839aa0fab4c3441f
Shuzhi Liu <liushuzhi.11235@icoud.cokm>
2020-01-02 23:48:38 +0900

    Fro RPN part, completed anchor_target. need to do: 1, bbox_reg_target 2, bbox_inside_weight 3, bbox_outside_weight 4, the method to efficiently calculate loss for model

--- NN_Helper/gen_train_target.py
+++ NN_Helper/gen_train_target.py
@@ -18,39 +18,58 @@ class gen_train_target():
         bboxes_ious = []    # for each gt_bbox calculate ious with candidates
         for bbox in bboxes:
             ious = bbox_tools.ious(self.anchor_generator.anchors_candidate_list, bbox)
-            ious[np.argmax(ious)] = 1
-            ious = np.array(ious).reshape((self.anchor_generator.h, self.anchor_generator.w, self.anchor_generator.n_anchors))
-            ious = np.where(ious>0.7, 1, ious)
-            ious = np.where(0.3<ious<0.7, -1, ious)
-            ious = np.where(ious<0.3, 0, ious)
-            bboxes_ious.append(ious)
-        train_anchors = None
-        train_bbox_reg = None
+            ious_temp = np.ones(shape=(len(ious)), dtype=np.float) * 0.5
+            # other author's implementations are use -1 to indicate ignoring, here use 0.5 to use max
+            ious_temp = np.where(np.asarray(ious)>0.7, 1, ious_temp)
+            ious_temp = np.where(np.asarray(ious)<0.3, 0, ious_temp)
+            ious_temp[np.argmax(ious)] = 1
+            bboxes_ious.append(ious_temp)
 
-        return train_anchors, train_bbox_reg
+        anchors_target = np.array(bboxes_ious)
+        anchors_target = np.max(anchors_target, axis=0)
+        anchors_target = np.reshape(anchors_target, newshape=(self.anchor_generator.h, self.anchor_generator.w, self.anchor_generator.n_anchors))
+        print(anchors_target.shape)
+        print(anchors_target[np.where(anchors_target==1)])
+        bbox_reg_target = None
+        bbox_inside_weight = None
+        bbox_outside_weight = None
+
+
+        return anchors_target, bbox_reg_target
 
 
     def _validate_bbox(self,image_id, bboxes):
         img1 = self.dataset.GetOriginalImage(image_id=image_id)
         for bbox in bboxes:
             color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-            cv.rectangle(img1, (bbox[1], bbox[0]), (bbox[3], bbox[2]), color, 4)
+            img1 = cv.rectangle(img1, (bbox[1], bbox[0]), (bbox[3], bbox[2]), color, 4)
         plt.imshow(img1)
+        plt.show()
+
+    def _validata_masks(self, image_id):
+        img1 = self.dataset.GetOriginalImage(image_id=image_id)
+        temp_img = np.zeros(shape=img1.shape, dtype=np.uint8)
+        Masks = self.dataset.GetOriginalSegmsMaskList(image_id=image_id)
+        for mask in Masks:
+            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            temp_img[:, :, 0][mask.astype(bool)] = color[0]
+            temp_img[:, :, 1][mask.astype(bool)] = color[1]
+            temp_img[:, :, 2][mask.astype(bool)] = color[2]
+        img1 = (img1 * 0.5 + temp_img * 0.5).astype(np.uint8)
+        plt.imshow(img1, cmap='gray')
         plt.show()
 
 
 
-BASE_PATH = '/Users/shuzhiliu/Google Drive/KyoceraRobotAI/mmdetection_tools/data'
+BASE_PATH = '/Users/liushuzhi/Google Drive/KyoceraRobotAI/mmdetection_tools/data'
 DATASET_ID = '1945415016934'
 
 t1 = gen_train_target(file=f"{BASE_PATH}/{DATASET_ID}/annotations/train.json",
                    imagefolder_path='/Users/shuzhiliu/Google Drive/KyoceraRobotAI/mmdetection_tools/LocalData_Images')
 bboxes = t1.dataset.GetOriginalBboxesList(image_id="20191119T063434-f7b72bed-b7ad-48c8-870a-7b4eaad23474")
-t1._validate_bbox(image_id="20191119T063434-f7b72bed-b7ad-48c8-870a-7b4eaad23474", bboxes=bboxes)
-Masks = t1.dataset.GetOriginalSegmsMaskList(image_id="20191119T063434-f7b72bed-b7ad-48c8-870a-7b4eaad23474")
-for mask in Masks:
-    plt.imshow(mask, cmap='gray')
-    plt.show()
+# t1._validate_bbox(image_id="20191119T063434-f7b72bed-b7ad-48c8-870a-7b4eaad23474", bboxes=bboxes)
+# t1._validata_masks(image_id="20191119T063434-f7b72bed-b7ad-48c8-870a-7b4eaad23474")
+t1.gen_train_target(image_id="20191119T063434-f7b72bed-b7ad-48c8-870a-7b4eaad23474")
 
 
 def test():
