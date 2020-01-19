Fixed the calculation bugs in _gen_train_data_RoI_one, added box_reg_target output. validated the RPN testing of training RPN and RoI simultaneously Next: 1, validate the RoI result 2, NMS tf implementation

FasterRCNN_NN.py
@@ -7,8 +7,9 @@ from Debugger import DebugPrint
 from FasterRCNN_config import Param
 import pickle
 import os
+import random
 
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+os.environ['KMP_DUPLICATE_LIB_OK']='True'   # for mac os tensorflow setting
 # tf.keras.backend.set_floatx('float64')
 
 class FasterRCNN():
@@ -23,7 +24,7 @@ class FasterRCNN():
         self.RPN_train_model = self.RPN.RPN_train_model
 
         # === RoI part ===
-        self.RoI = RoI(self.backbone_model, self.IMG_SHAPE, Param.N_STAGE)
+        self.RoI = RoI(self.backbone_model, self.IMG_SHAPE, Param.LR, Param.N_STAGE)
         self.RoI_train_model = self.RoI.RoI_train_model
 
         # === Data part ===
@@ -202,7 +203,7 @@ class FasterRCNN():
     def train_RoI(self):
         for _ in range(100):
             x,y = self.train_data_generator.gen_train_data_RoI_generator()
-            self.RoI.train_step(x[0][:2],x[1][:2],y[0][:2],y[1][:2])
+            self.RoI.train_step_with_backbone(x[0][:2], x[1][:2], y[0][:2], y[1][:2])
             print('trained one step')
             # self.RoI_train_model.fit(x=x,
             #                          y=y,
@@ -214,6 +215,7 @@ class FasterRCNN():
 
     def train_RPN_RoI(self,load_data=False):
         inputs, anchor_targets, bbox_reg_targets = self.get_train_data_RPN(load_data)
+        # print(f"box reg target shape: {bbox_reg_targets.shape}")
         n_sample = inputs.shape[0]
         image_ids = self.train_data_generator.dataset_coco.image_ids
         for epoch in range(Param.EPOCH):
@@ -221,11 +223,13 @@ class FasterRCNN():
             for i in range(n_sample):
                 # print(f'{i} th image')
                 self.RPN.train_step(inputs[i:i + 1], anchor_targets[i:i + 1], bbox_reg_targets[i:i + 1])
-                input_img, tar_an_b, tar_cls = self.train_data_generator._gen_train_data_RoI_one(image_ids[i])
-                input_img, tar_an_b, tar_cls = np.asarray(input_img).astype(np.float), np.asarray(tar_an_b), np.asarray(tar_cls)
+                input_img, input_box_fromAnchorBox, target_class, target_bbox_reg = self.train_data_generator._gen_train_data_RoI_one(image_ids[i])
+                input_img, input_box_fromAnchorBox, target_class, target_bbox_reg = np.asarray(input_img).astype(np.float), np.asarray(input_box_fromAnchorBox), np.asarray(target_class), np.asarray(target_bbox_reg)
                 n_box = input_img.shape[0]
                 for j in range(n_box):
-                    self.RoI.train_step(input_img[j:j+1], tar_an_b[j:j+1], tar_cls[j:j+1], bbox_reg_targets[i][j:j+1])
+                    self.RoI.train_step_no_backbone(input_img[j:j + 1], input_box_fromAnchorBox[j:j + 1], target_class[j:j + 1], target_bbox_reg[j:j + 1])
+                j = random.randint(a=0, b=n_box-1)    # model with backbone only be trained once to balance RPN and RoI training
+                self.RoI.train_step_with_backbone(input_img[j:j + 1], input_box_fromAnchorBox[j:j + 1], target_class[j:j + 1], target_bbox_reg[j:j + 1])
 
 
     def save_weight(self):
@@ -245,8 +249,8 @@ if __name__ == '__main__':
     # print(t1, t2)
     # f1.test_proposal_visualization()
     # f1.train_RPN(load_data=True)
-    # f1.train_RPN_RoI(load_data=True)
-    # f1.save_weight()
+    f1.train_RPN_RoI(load_data=True)
+    f1.save_weight()
     f1.load_weight()
     f1.test_proposal_visualization()
     # f1.test_RoI_visualization()

NN_Helper/nn_data_generator.py
@@ -132,11 +132,11 @@ class NN_data_generator():
         return target_anchor_bboxes, target_classes
 
     def _gen_train_data_RoI_one(self,image_id):
-        bboxes = self.dataset_coco.GetOriginalBboxesList(image_id=image_id)
+        gt_bboxes = self.dataset_coco.GetOriginalBboxesList(image_id=image_id)
         SparseTargets = self.dataset_coco.GetOriginalCategorySparseList(image_id=image_id)
 
         bboxes_ious = []  # for each gt_bbox calculate ious with candidates
-        for bbox in bboxes:
+        for bbox in gt_bboxes:
             ious = bbox_tools.ious(self.anchor_generator.anchor_candidates_list, bbox)
             ious_temp = np.ones(shape=(len(ious)), dtype=np.float) * 0.5
             # other author's implementations are use -1 to indicate ignoring, here use 0.5 to use max
@@ -148,25 +148,30 @@ class NN_data_generator():
         # for each gt_box, determine the box reg target
         original_img = self.gen_train_input(image_id)
         input_images = []
-        target_anchor_bboxes = []
+        input_box_fromAnchorBox = []
         target_classes = []
+        target_bbox_reg = []
         for index, bbox_ious in enumerate(bboxes_ious):
-            ious_temp = np.reshape(bbox_ious, newshape=(
-            self.anchor_generator.h, self.anchor_generator.w, self.anchor_generator.n_anchors))
+            ious_temp = np.reshape(bbox_ious, newshape=(self.anchor_generator.h,
+                                                        self.anchor_generator.w,
+                                                        self.anchor_generator.n_anchors))
             candidate_boxes = self.anchor_generator.anchor_candidates[np.where(ious_temp == 1)]
             n = candidate_boxes.shape[0]
             for i in range(n):
-                target_anchor_bboxes.append(candidate_boxes[i].astype(np.float))
+                input_box_fromAnchorBox.append(candidate_boxes[i].astype(np.float))
+                box_reg = bbox_tools.bbox_regression_target(pred_boxes=candidate_boxes[i].reshape((1,4)), gt_box=gt_bboxes[index])
+                target_bbox_reg.append(box_reg.ravel())
                 target_classes.append(SparseTargets[index])
                 input_images.append(original_img.astype(np.float))
-        return input_images ,target_anchor_bboxes, target_classes
+        return input_images ,input_box_fromAnchorBox, target_classes, target_bbox_reg
 
     def gen_train_data_RoI_generator(self):
+        # abandoned already, find a time to clean this part
         input_images, target_anchor_bboxes, target_classes, bbox_reg_targets = [], [], [], []
         n_len = 80
         while len(input_images) < n_len:
             image_id = random.choice(self.dataset_coco.image_ids)
-            input_img, tar_an_b, tar_cls = self._gen_train_data_RoI_one(image_id)
+            input_img, tar_an_b, tar_cls, bbox_reg_target_RoI = self._gen_train_data_RoI_one(image_id)
             input_images += input_img
             target_anchor_bboxes += tar_an_b
             target_classes += tar_cls

NN_Parts/RoI.py
@@ -37,7 +37,7 @@ class RoI:
         self.optimizer = tf.keras.optimizers.Adam(self.lr)
 
     @tf.function
-    def train_step(self, input_image, proposal_box, class_header, box_reg_header):
+    def train_step_with_backbone(self, input_image, proposal_box, class_header, box_reg_header):
         with tf.GradientTape() as RoI_tape:
             class_pred, box_reg_pred = self.RoI_train_model([input_image, proposal_box])
             class_loss = tf.keras.losses.sparse_categorical_crossentropy(y_true=class_header, y_pred=class_pred)
@@ -46,6 +46,17 @@ class RoI:
             total_loss = tf.reduce_mean(tf.add(class_loss, box_reg_loss))
         gradients = RoI_tape.gradient(total_loss, self.RoI_train_model.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients, self.RoI_train_model.trainable_variables))
+
+    @tf.function
+    def train_step_no_backbone(self, input_image, proposal_box, class_header, box_reg_header):
+        with tf.GradientTape() as RoI_tape:
+            class_pred, box_reg_pred = self.RoI_train_model([input_image, proposal_box])
+            class_loss = tf.keras.losses.sparse_categorical_crossentropy(y_true=class_header, y_pred=class_pred)
+
+            box_reg_loss = self.huber(y_true=box_reg_header, y_pred=box_reg_pred)
+            total_loss = tf.reduce_mean(tf.add(class_loss, box_reg_loss))
+        gradients = RoI_tape.gradient(total_loss, self.RoI_header_model.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.RoI_header_model.trainable_variables))
 
 
 if __name__ == '__main__':