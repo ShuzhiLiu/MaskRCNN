01808aeaf2602397dbe8679ea9aa92d01d733d34
ShuzhiLiu <liushuzhi2014@gmail.com>
2020-01-19 21:40:00 +0900

    Refactored RoI to two models: RoI header model and RoI train model

--- NN_Parts/RoI.py
+++ NN_Parts/RoI.py
@@ -6,26 +6,30 @@ from NN_Parts import Backbone
 
 class RoI:
     def __init__(self, backbone_model, IMG_SHAPE, lr=1e-4, n_stage=5):
-        self.base_model = backbone_model
+        self.backbone_model = backbone_model
         self.lr = lr
         proposal_boxes = tf.keras.Input(shape=(4,), batch_size=None, name='PROPOSAL_BOXES')
+        feature_map_shape = self.backbone_model.layers[-1].output_shape[1:]
+        feature_map = tf.keras.Input(shape=feature_map_shape, name='FEATURE_MAP')
+
         shape1 = tf.shape(proposal_boxes)
         n_boxes = tf.gather_nd(shape1, [0])
         indices = tf.range(start=0, limit=n_boxes, dtype=tf.int32)
         img_shape_constant = tf.constant([IMG_SHAPE[0], IMG_SHAPE[1], IMG_SHAPE[0], IMG_SHAPE[1]], tf.float32)
         proposal_boxes2 = tf.math.divide(proposal_boxes, img_shape_constant)
 
-        image_crop = tf.image.crop_and_resize(self.base_model.output, proposal_boxes2, indices, [6, 6])
+        image_crop = tf.image.crop_and_resize(feature_map, proposal_boxes2, indices, [6, 6])
         flatten1 = tf.keras.layers.GlobalAveragePooling2D()(image_crop)
         fc1 = tf.keras.layers.Dense(units=1024, activation='relu')(flatten1)
         # fc2 = tf.keras.layers.Dense(units=2048, activation='relu')(fc1)
         class_header = tf.keras.layers.Dense(units=81, activation='softmax')(fc1)
         box_reg_header = tf.keras.layers.Dense(units=4, activation='linear')(fc1)
 
-        self.RoI_train_model = tf.keras.Model(inputs=[self.base_model.input, proposal_boxes],
-                                              outputs=[class_header, box_reg_header])
-        # self.RoI_train_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=Param.LR),
-        #                              loss=['sparse_categorical_crossentropy','huber_loss'], loss_weights=[1,10])
+        self.RoI_header_model = tf.keras.Model(inputs=[feature_map, proposal_boxes],
+                                               outputs=[class_header, box_reg_header])
+        RoI_with_backbone_out1,RoI_with_backbone_out2 = self.RoI_header_model([self.backbone_model.output, proposal_boxes])
+        self.RoI_train_model = tf.keras.Model(inputs=[self.backbone_model.input, proposal_boxes],
+                                              outputs=[RoI_with_backbone_out1, RoI_with_backbone_out2])
         tf.keras.utils.plot_model(self.RoI_train_model, 'RoI_with_backbone.png', show_shapes=True)
 
         # --- for train step ---
